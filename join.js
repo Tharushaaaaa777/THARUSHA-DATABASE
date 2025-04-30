d7e231ed614999a447e855e2950f268d83265939
Tharushaaaaa777 <tharushasandipa250@gmail.com>
2025-04-30 14:52:30 +0530

    Add files via upload

---
+++ join.js
@@ -0,0 +1,52 @@
+{
+
+"BOT_NAME": "HORIZON MD",
+"VERSION": "3.0.0",
+"CREATED BY": "TECH HORIZON",
+  
+"replyMsg": {
+  
+  "giveme": "*Type something, what do you think, who do you think I look like?*",
+
+  "giveut": "*Give a link or a text 😒*",
+
+  "not_fo": "*❌ I Couldn't find anything :(*",
+
+  "url": "*Give me a link 😐*",
+  
+  "valid_url": "*This Url is invalid ❌ Please give me valid Url ❗*",
+
+  "size_big": "*File size is too big ❌*",
+
+  "own_cmd": "*This is Owner Commad ‼*",
+
+  "pre_cmd": "`Your not premium user` 🚫\n\n* Contact to buy a premium connection for Rs.100  for a month \n\n* https://wa.me/923427582273?text=GET_PREMIUIM_CONNECTION👾",
+
+  "ald_set": "*Already set* 🙂✅",
+
+  "only_gp": "This is group command ❗",
+
+  "inb_cmd": "This is inbox command ❗",
+
+  "give_adm": "*Give admin first 😹💪🏻*",
+
+  "you_adm": "You`re Must Be Admin First 😒",
+
+  "need_jid": "Please give me jid ❓",
+
+  "valid_jid": "*This not jid ❌ Please give me valid jid ❗*",
+
+  "ald_approve_jid": "Jid approve accept ✔",
+
+  "ald_approve_alljid": "All Jid approve accept ✔",
+
+  "ald_reapprove_jid": "Jid approve reject ✔",
+
+  "ald_reapprove_alljid": "All Jid approve reject ✔",
+
+  "band_users": "❌ *You are banned from using Commands.....*\n\n*_Please contact UD MD Bot Owner Remove your Ban_* 👨‍🔧\n",
+
+  "erro": "Error" 
+}
+
+    }
